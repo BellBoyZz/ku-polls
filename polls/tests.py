03857f99af682e048078c507cc6c5b5cf4657a95
peerasu <peerasu.w@ku.th>
2020-09-16 23:14:28 +0700

    Created unit tests for the two new methods

--- polls/tests.py
+++ polls/tests.py
@@ -95,6 +95,38 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+    def test_is_published_with_past_question(self):
+        """
+        is_published() returns True for questions whose pub_date is in the past.
+        """
+        time = timezone.now() - datetime.timedelta(days=2)
+        past_question = Question(pub_date=time)
+        self.assertIs(past_question.is_published(), True)
+
+    def test_is_published_with_future_question(self):
+        """
+        is_published() returns False for questions whose pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(hours=5)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.is_published(), False)
+
+    def test_can_vote_with_past_question(self):
+        """
+        can_vote() returns False for questions that are closed.
+        """
+        time = timezone.now() - datetime.timedelta(days=4)
+        past_question = Question(pub_date=time)
+        self.assertIs(past_question.can_vote(), False)
+
+    def test_can_vote_with_future_question(self):
+        """
+        can_vote() returns False for questions that are not open yet.
+        """
+        time = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.can_vote(), False)
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
