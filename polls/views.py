5bf00b23fcca54e3a03af9aacf8cbc1d83951cd3
peerasu <peerasu.w@ku.th>
2020-09-18 10:26:52 +0700

    Edit the index page to show all published poll questions and sort them

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ class IndexView(generic.ListView):
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:]
 
 
 class DetailView(generic.DetailView):
@@ -37,7 +37,7 @@ class ResultsView(generic.DetailView):
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
