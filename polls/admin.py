5c6ae6ad0000c5fda104b98e533e245a51685270
peerasu <peerasu.w@ku.th>
2020-10-08 01:25:13 +0700

    Edit some code to use flake8

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
+"""Admin site for KU-Polls."""
+
 from django.contrib import admin
 
 from .models import Choice, Question
@@ -5,18 +7,23 @@ from .models import Choice, Question
 # Register your models here.
 
 class ChoiceInline(admin.TabularInline):
+    """Class for in-line choice."""
+
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """Class for admin question."""
+
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date','end_date'], 'classes': ['collapse']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
-    search_fields = ['question_text'] 
+    search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
